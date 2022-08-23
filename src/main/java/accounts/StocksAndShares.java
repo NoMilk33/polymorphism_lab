4b665814b644fb0734d929a015fd477f494dfadc
Kat Fagg <katjfagg@gmail.com>
2022-08-23 21:52:52 +0100

    restart with new classes

--- src/main/java/accounts/StocksAndShares.java
+++ src/main/java/accounts/StocksAndShares.java
@@ -1,16 +1,1 @@
-package accounts;
 
-public class StocksAndShares extends Investment {
-
-    public StocksAndShares (String name){
-        super(name);
-    }
-    
-    public void depositCash(){
-
-    }
-
-    public void withdrawCash(){
-
-    }
-}
